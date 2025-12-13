bff9d421f4dec5858d1a0d03f3e46a213ab87201
drsunshine80-coder <dr.sunshine80@gmail.com>
2025-12-13 16:47:38 -0600

    Component Filder - Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import "./globals.css";
-import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,24 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-          <div>
-            <Button asChild variant="ghost">
-              <Link href={homePath()}>
-                <LucideKanban />
-                <h1 className="ml-2 text-lg font-semibold">TicketBounty</h1>
-              </Link>
-            </Button>
-          </div>
-          <div>
-            <Link
-              href={ticketsPath()}
-              className={buttonVariants({ variant: "outline" })}
-            >
-              Tickets
-            </Link>
-          </div>
-        </nav>
+        <Header />
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
           {children}
         </main>
