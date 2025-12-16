47560955c09a3f9084dac3c1676f9e3a3574e839
drsunshine80-coder <dr.sunshine80@gmail.com>
2025-12-15 21:29:26 -0600

    Feature Folder - Extract

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,6 @@
-import clsx from "clsx";
-import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText />,
-  IN_PROGRESS: <LucidePencil />,
-  DONE: <LucideCircleCheck />,
-};
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
 const TicketsPage = () => {
   return (
@@ -25,24 +9,7 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2 items-center truncate">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <h3 className="text-2xl">{ticket.title}</h3>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </p>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
