6fc226095b33359144d2abd829f8cf12cb0e419d
drsunshine80-coder <dr.sunshine80@gmail.com>
2025-12-26 21:26:33 -0600

    Forms with Server Actions - DRY with Abstractions

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -3,19 +3,23 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
-import { ticketsPath } from "@/paths";
+import { ticketPath, ticketsPath } from "@/paths";
 
-export const updateTicket = async (id: string, formData: FormData) => {
+export const upsertTicket = async (id: string | undefined, formData: FormData) => {
   const data = {
     title: formData.get("title") as string,
     content: formData.get("content") as string,
   };
 
-  await prisma.ticket.update({
-    where: { id },
-    data,
+  await prisma.ticket.upsert({
+    where: { id: id ?? "" },
+    create: data,
+    update: data,
   });
 
   revalidatePath(ticketsPath());
-  redirect(ticketsPath());
+
+  if (id) {
+    redirect(ticketPath(id));
+  }
 }
