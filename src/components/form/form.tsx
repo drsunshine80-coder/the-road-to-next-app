b7ee63e5b207d78c23e61219c0dca32f3da04ab4
drsunshine80-coder <dr.sunshine80@gmail.com>
2026-01-27 21:44:56 -0600

    Advanced UIs - Exposing Callback Handlers

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,3 +1,4 @@
+import { on } from "events";
 import { toast } from "sonner";
 import { useActionFeedback } from "./hooks/use-action-feedback";
 import { type ActionState } from "./utils/to-action-state";
@@ -6,17 +7,29 @@ type FormProps = {
   action: (payload: FormData) => void;
   actionState: ActionState;
   children: React.ReactNode;
+  onSuccess?: (actionState: ActionState) => void;
+  onError?: (actionState: ActionState) => void;
 };
 
-const Form = ({ action, actionState, children }: FormProps) => {
+const Form = ({
+  action,
+  actionState,
+  children,
+  onSuccess,
+  onError,
+}: FormProps) => {
   useActionFeedback(actionState, {
     onSuccess: ({ actionState }) => {
       toast.success(actionState.message);
+
+      onSuccess?.(actionState);
     },
     onError: ({ actionState }) => {
       if (actionState.message) {
         toast.error(actionState.message);
       }
+
+      onError?.(actionState);
     },
   });
 
